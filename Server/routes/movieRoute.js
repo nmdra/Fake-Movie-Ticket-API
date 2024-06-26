0973eab1328bc3fa83d88ae39be47574b0cf7660
nmdra <nimendradilshan11@gmail.com>
2024-06-26 12:16:13 +0530

    Add APICache

--- Server/routes/movieRoute.js
+++ Server/routes/movieRoute.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import apicache from 'apicache';
 import {
     addMovie,
     getAllMovies,
@@ -7,9 +8,10 @@ import {
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
+const cache = apicache.middleware; 
 
-router.route('/add').post(protect, addMovie); // admin only
-router.route('/').get(protect,getAllMovies); 
-router.route('/:imdbId').get(protect, getMovieByImdbId); 
+router.route('/add').post(protect,addMovie); // admin only
+router.route('/').get(protect,cache('5 minutes'), getAllMovies); 
+router.route('/:imdbId').get(protect,cache('5 minutes'), getMovieByImdbId); 
 
 export default router;
